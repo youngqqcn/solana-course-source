ebb7756e81fa28c8e2f90c9d34a46e15d9ade966
yqq <youngqqcn@gmail.com>
2024-07-11 18:20:31 +0800

    update

--- 1_onchain_program_development/move-frontend/utils/constants.ts
+++ 1_onchain_program_development/move-frontend/utils/constants.ts
@@ -1,2 +1,3 @@
 export const MOVIE_REVIEW_PROGRAM_ID =
-    "6y8n4sqpgurkF9H1mabrkC5m5uzVrA1FVv5Pt9QzaBho";
+    "8LDRWRbLrHQhqfKd9MTWegvppHsZB9dM34yJLZKVfwGp";
+    // "6y8n4sqpgurkF9H1mabrkC5m5uzVrA1FVv5Pt9QzaBho";
