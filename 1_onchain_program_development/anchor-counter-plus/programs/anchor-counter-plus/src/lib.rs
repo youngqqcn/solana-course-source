b34fbb95a7d924caad268c2b9562621d5098ce85
yqq <youngqqcn@gmail.com>
2024-07-12 15:19:14 +0800

    基于anchor的计数器，自己实现

--- 1_onchain_program_development/anchor-counter-plus/programs/anchor-counter-plus/src/lib.rs
+++ 1_onchain_program_development/anchor-counter-plus/programs/anchor-counter-plus/src/lib.rs
@@ -4,13 +4,59 @@ declare_id!("Hm5ATvxsAvrrrUrgof6dyCLP3V4NQkATpCfQpmQzXjCk");
 
 #[program]
 pub mod anchor_counter_plus {
+
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+        let counter = &mut ctx.accounts.counter_account;
+        counter.counter = 0;
+        msg!("initialized with 0");
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter_account;
+        msg!("previous : {:?}", counter.counter);
+        counter.counter = counter.counter.checked_add_signed(1).unwrap();
+        msg!("after increment: {:?}", counter.counter);
+        Ok(())
+    }
+
+    pub fn decrement(ctx: Context<Decrement>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter_account;
+        msg!("previous : {:?}", counter.counter);
+        counter.counter = counter.counter.checked_add_signed(-1).unwrap();
+        msg!("update decrement: {:?}", counter.counter);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer=user, space=8+8)]
+    pub counter_account: Account<'info, CounterPlus>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub counter_account: Account<'info, CounterPlus>,
+    // #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Decrement<'info> {
+    #[account(mut)]
+    pub counter_account: Account<'info, CounterPlus>,
+    pub user: Signer<'info>,
+}
+
+#[account]
+pub struct CounterPlus {
+    counter: u64,
+}
--- 1_onchain_program_development/anchor-counter-plus/tests/anchor-counter-plus.ts
+++ 1_onchain_program_development/anchor-counter-plus/tests/anchor-counter-plus.ts
@@ -1,16 +1,66 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorCounterPlus } from "../target/types/anchor_counter_plus";
+import { expect } from "chai";
 
 describe("anchor-counter-plus", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    // Configure the client to use the local cluster.
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
 
-  const program = anchor.workspace.AnchorCounterPlus as Program<AnchorCounterPlus>;
+    const program = anchor.workspace
+        .AnchorCounterPlus as Program<AnchorCounterPlus>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+    const counterPDA = anchor.web3.Keypair.generate();
+    it("initialized!", async () => {
+        // Add your test here.
+        const tx = await program.methods
+            .initialize()
+            .accounts({
+                counterAccount: counterPDA.publicKey,
+            })
+            .signers([counterPDA])
+            .rpc();
+        console.log("Your transaction signature", tx);
+
+        let counter = await program.account.counterPlus.fetch(
+            counterPDA.publicKey
+        );
+
+        expect(counter.counter.toNumber()).to.equal(0);
+    });
+
+    it("increment", async () => {
+        let tx = await program.methods
+            .increment()
+            .accounts({
+                counterAccount: counterPDA.publicKey,
+                user: provider.publicKey,
+            })
+            .signers([])
+            .rpc();
+
+        let counter = await program.account.counterPlus.fetch(
+            counterPDA.publicKey
+        );
+
+        expect(counter.counter.toNumber()).to.equal(1);
+    });
+
+    it("decrements the counter", async () => {
+        await program.methods
+            .decrement()
+            .accounts({
+                counterAccount: counterPDA.publicKey,
+                user: provider.publicKey,
+            })
+            .signers([])
+            .rpc();
+
+        expect(
+            (
+                await program.account.counterPlus.fetch(counterPDA.publicKey)
+            ).counter.toNumber()
+        ).to.equal(0);
+    });
 });
