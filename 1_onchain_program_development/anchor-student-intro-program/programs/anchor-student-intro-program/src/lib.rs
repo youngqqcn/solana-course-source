9b367ec91d5736ca118462ffd555e4899751bff9
yqq <youngqqcn@gmail.com>
2024-07-13 14:56:31 +0800

    update

--- 1_onchain_program_development/anchor-student-intro-program/programs/anchor-student-intro-program/src/lib.rs
+++ 1_onchain_program_development/anchor-student-intro-program/programs/anchor-student-intro-program/src/lib.rs
@@ -4,13 +4,81 @@ declare_id!("9eyVUWKL7jXk5YH1pY8VyJPfXR5vDd9vksaKcxFpX4xt");
 
 #[program]
 pub mod anchor_student_intro_program {
+    use std::io::Result;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_student_intro(
+        ctx: Context<CreateStudentIntro>,
+        name: String,
+        intro: String,
+    ) -> Result<()> {
+        msg!("student_intro account created!");
+        msg!("name: {}", name);
+        msg!("intro : {}", intro);
+
+        let student_intro = &mut ctx.accounts.student_intro_account;
+
+        student_intro.name = name;
+        student_intro.intro = intro;
+
+        Ok(())
+    }
+
+    pub fn update_student_intro(
+        ctx: Context<UpdateStudentIntro>,
+        name: String,
+        intro: String,
+    ) -> Result<()> {
+        let student_intro = &mut ctx.accounts.student_intro_account;
+        student_intro.name = name;
+        student_intro.intro = intro;
+
+        msg!("student intro updated");
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(name: String, intro: String)]
+pub struct CreateStudentIntro<'info> {
+    #[account(
+        init,
+        payer=initializer,
+        space=StudentIntroState::INIT_SPACE + name.len() + intro.len(),
+        seeds=[name.as_bytes(), initializer.key().as_ref()],
+        bump,
+    )]
+    pub student_intro_account: Account<'info, StudentIntroState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(name: String, intro: String)]
+pub struct UpdateStudentIntro<'info> {
+    #[account(
+        mut,
+        seeds=[name.as_bytes(), initializer.key().as_ref()],
+        bump,
+        realloc=StudentIntroState::INIT_SPACE + name.len() + intro.len(),
+        realloc::payer=initializer,
+        realloc::zero=true,
+    )]
+    pub student_intro_account: Account<'info, StudentIntroState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+
+#[account]
+pub struct StudentIntroState {
+    pub name: String,
+    pub intro: String,
+}
+
+impl Space for StudentIntroState {
+    const INIT_SPACE: usize = 8 + 4 + 4;
+}
--- 1_onchain_program_development/anchor-student-intro-program/tests/anchor-student-intro-program.ts
+++ 1_onchain_program_development/anchor-student-intro-program/tests/anchor-student-intro-program.ts
@@ -1,16 +1,46 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorStudentIntroProgram } from "../target/types/anchor_student_intro_program";
+import { expect } from "chai";
 
 describe("anchor-student-intro-program", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    // Configure the client to use the local cluster.
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
 
-  const program = anchor.workspace.AnchorStudentIntroProgram as Program<AnchorStudentIntroProgram>;
+    const program = anchor.workspace
+        .AnchorStudentIntroProgram as Program<AnchorStudentIntroProgram>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+    const stuIntro = {
+        name: "Alice",
+        intro: "intro",
+    };
+
+    const [studentPda, _] = anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from(stuIntro.name), provider.wallet.publicKey.toBytes()],
+        program.programId
+    );
+
+    it("create student intro", async () => {
+        // Add your test here.
+        const tx = await program.methods
+            .createStudentIntro(stuIntro.name, stuIntro.intro)
+            .rpc();
+
+        let studentIntroAccount = await program.account.studentIntroState.fetch(
+            studentPda
+        );
+
+        expect(studentIntroAccount.intro, stuIntro.intro);
+        expect(studentIntroAccount.name, stuIntro.name);
+    });
+
+    it("update student intro", async () => {
+        await program.methods.updateStudentIntro("updated name", "new intro");
+
+        let stuAcc = await program.account.studentIntroState.fetch(studentPda);
+
+        expect(stuAcc.name, stuIntro.name);
+        expect(stuAcc.intro, stuIntro.intro);
+    });
 });
