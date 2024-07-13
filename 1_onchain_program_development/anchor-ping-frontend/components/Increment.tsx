593d76a4447754fe6574795df829cddcc57e204d
yqq <youngqqcn@gmail.com>
2024-07-13 08:12:25 +0800

    update

--- 1_onchain_program_development/anchor-ping-frontend/components/Increment.tsx
+++ 1_onchain_program_development/anchor-ping-frontend/components/Increment.tsx
@@ -1,35 +1,69 @@
 import {
-  useConnection,
-  useWallet,
-  useAnchorWallet,
-} from "@solana/wallet-adapter-react"
-import * as anchor from "@project-serum/anchor"
-import { FC, useCallback, useEffect, useState } from "react"
-import idl from "../idl.json"
-import { Button, HStack, VStack, Text } from "@chakra-ui/react"
+    useConnection,
+    useWallet,
+    useAnchorWallet,
+} from "@solana/wallet-adapter-react";
+import * as anchor from "@project-serum/anchor";
+import { FC, useCallback, useEffect, useState } from "react";
+import idl from "../idl.json";
+import { Button, HStack, VStack, Text } from "@chakra-ui/react";
 
-const PROGRAM_ID = `9pbyP2VX8Rc7v4nR5n5Kf5azmnw5hHfkJcZKPHXW98mf`
+const PROGRAM_ID = `9pbyP2VX8Rc7v4nR5n5Kf5azmnw5hHfkJcZKPHXW98mf`;
 
 export interface Props {
-  counter
-  setTransactionUrl
+    counter;
+    setTransactionUrl;
 }
 
 export const Increment: FC<Props> = ({ counter, setTransactionUrl }) => {
-  const [count, setCount] = useState(0)
-  const [program, setProgram] = useState<anchor.Program>()
+    const [count, setCount] = useState(0);
+    const [program, setProgram] = useState<anchor.Program>();
+    const { connection } = useConnection();
+    const wallet = useAnchorWallet();
 
-  const incrementCount = async () => {}
+    useEffect(() => {
+        let provider: anchor.Provider;
 
-  const refreshCount = async (program) => {}
+        try {
+            provider = anchor.getProvider();
+        } catch (error) {
+            provider = new anchor.AnchorProvider(connection, wallet, {});
+            anchor.setProvider(provider);
+        }
 
-  return (
-    <VStack>
-      <HStack>
-        <Button onClick={incrementCount}>Increment Counter</Button>
-        <Button onClick={() => refreshCount(program)}>Refresh count</Button>
-      </HStack>
-      <Text color="white">Count: {count}</Text>
-    </VStack>
-  )
-}
+        const program = new anchor.Program(idl as anchor.Idl, PROGRAM_ID);
+        setProgram(program);
+    }, [connection, wallet]);
+
+    const incrementCount = async () => {
+        const sig = await program.methods
+            .increment()
+            .accounts({
+                counter: counter,
+                user: wallet.publicKey,
+            })
+            .signers([])
+            .rpc();
+
+        setTransactionUrl(
+            `https://explorer.solana.com/tx/${sig}?cluster=devnet`
+        );
+    };
+
+    const refreshCount = async (program: anchor.Program) => {
+        let counterAccount = await program.account.counter.fetch(counter);
+        setCount((counterAccount.count as anchor.BN).toNumber());
+    };
+
+    return (
+        <VStack>
+            <HStack>
+                <Button onClick={incrementCount}>Increment Counter</Button>
+                <Button onClick={() => refreshCount(program)}>
+                    Refresh count
+                </Button>
+            </HStack>
+            <Text color="white">Count: {count}</Text>
+        </VStack>
+    );
+};
