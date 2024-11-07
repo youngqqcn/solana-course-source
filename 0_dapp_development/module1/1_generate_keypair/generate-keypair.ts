16f27ef90edffae8a8c12e21412d557b1f34a726
yqq <youngqqcn@gmail.com>
2024-11-07 14:55:57 +0800

    update

--- 0_dapp_development/module1/1_generate_keypair/generate-keypair.ts
+++ 0_dapp_development/module1/1_generate_keypair/generate-keypair.ts
@@ -1,14 +1,17 @@
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 import { Keypair } from "@solana/web3.js";
 import "dotenv/config"
+import bs58 from "bs58"
 
 // 生成新的keypair
-const keypair = Keypair.generate();
-console.log("public key: ", keypair.publicKey.toBase58());
-console.log("private key: ", keypair.secretKey);
+// const keypair = Keypair.generate();
+// console.log("public key: ", keypair.publicKey.toBase58());
+// console.log("private key: ", keypair.secretKey);
 
 // 从.env中加载已有的keypair
 const keypair_local = getKeypairFromEnvironment("SECRET_KEY");
 console.log("local public key: ", keypair_local.publicKey.toBase58());
 console.log("local private key: ", keypair_local.secretKey);
+console.log("local private key base58 : ", bs58.encode( keypair_local.secretKey))
+
 
